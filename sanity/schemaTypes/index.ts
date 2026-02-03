b700820e72ee6c9b382cff8a43382e017bb01e6e
Umer Shahid <umershahid123@gmail.com>
2026-02-03 12:05:58 +0500

    remove Sanity dependencies and configuration file.

--- sanity/schemaTypes/index.ts
+++
@@ -1,7 +0,0 @@
-import { type SchemaTypeDefinition } from 'sanity'
-import { project } from './project'
-import { skill } from './skill'
-
-export const schema: { types: SchemaTypeDefinition[] } = {
-    types: [project, skill],
-}
